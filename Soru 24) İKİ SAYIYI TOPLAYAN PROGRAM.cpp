9c58ea11f97b8c2f22f349b9524e4b7cec2e8b93
Olcay-Berberoglu <84090885+Olcay-Berberoglu@users.noreply.github.com>
2021-10-19 16:04:33 +0300

    İKİ SAYIYI TOPLAYAN PROGRAM

---
+++ Soru 24) İKİ SAYIYI TOPLAYAN PROGRAM.cpp
@@ -0,0 +1,14 @@
+//  ÝKÝ SAYIYI TOPLAYAN PROGRAM
+
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+	
+	
+	int sayi1=10,sayi2=5,top=0;
+	top=sayi1+sayi2;
+	printf("SONUC = %d",top);
+	
+	
+	
+      }
