3e0a52b366d75c8c6dc4da3e9b75e9c06f6e7e6d
Olcay-Berberoglu <84090885+Olcay-Berberoglu@users.noreply.github.com>
2021-10-19 16:14:56 +0300

    Kenar uzunlukları kullanıcı tarafından girilen bir dikdörtgenin alanını veren program

---
+++ Soru 28) Kenar uzunlukları kullanıcı tarafından girilen bir dikdörtgenin alanını veren program.cpp
@@ -0,0 +1,18 @@
+//  Kenar uzunluklarý kullanýcý tarafýndan girilen bir dikdörtgenin alanýný veren program 
+
+
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+	int k1,k2,alan=0;
+	
+	printf("1.kenar gir = ");
+	scanf("%d",&k1);
+	printf("2.kenar gir = ");
+	scanf("%d",&k2);
+	alan=k1*k2;
+	printf("%d",alan);
+	
+
+
+      }
