4059088652c9fc5532f95f9bfae3c4b63b3815f1
Olcay-Berberoglu <84090885+Olcay-Berberoglu@users.noreply.github.com>
2021-10-19 15:37:18 +0300

    Girdiğiniz adiniz ile merhaba kelimesini birleştiren program

---
+++ Soru 3) Girdiğiniz adiniz ile merhaba kelimesini birleştiren program.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Girdiðiniz adiniz ile merhaba kelimesini birleþtiren program
+
+ main (){
+
+	char isim [10];
+	printf("Adinizi giriniz :");
+	scanf("%s",isim );
+	printf ("merhaba %s !",isim);
+	
+	}
