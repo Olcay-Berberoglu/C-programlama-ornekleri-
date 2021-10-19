7e43b086d63f3ac5deb046a4e13b5c1be6023bce
Olcay-Berberoglu <84090885+Olcay-Berberoglu@users.noreply.github.com>
2021-10-19 16:17:55 +0300

    Kullanıcı tarafından girilen iki sayının kareler toplamını hesaplayan ve yazdıran programın

---
+++ Soru 32) Kullanıcı tarafından girilen iki sayının kareler toplamını hesaplayan ve yazdıran programın.cpp
@@ -0,0 +1,16 @@
+// Kullanýcý tarafýndan girilen iki sayýnýn kareler toplamýný hesaplayan ve yazdýran programýn 
+
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+	int sonuc=0,sayi1,sayi2;
+printf("sayi1 gir = ");
+	scanf("%d",&sayi1);
+	printf("sayi2 gir = ");
+	scanf("%d",&sayi2);
+sonuc=(sayi1*sayi1)+(sayi2*sayi2);
+printf("%d",sonuc);
+ 
+	
+	
+      }
