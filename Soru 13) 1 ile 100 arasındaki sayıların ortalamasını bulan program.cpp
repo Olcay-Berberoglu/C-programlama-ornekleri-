398741199e66dc3bd98f702137443b16ae42f7ec
Olcay-Berberoglu <84090885+Olcay-Berberoglu@users.noreply.github.com>
2021-10-19 15:56:02 +0300

    1 ile 100 arasındaki sayıların ortalamasını bulan program

---
+++ Soru 13) 1 ile 100 arasındaki sayıların ortalamasını bulan program.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	// 1 ile 100 arasýndaki sayýlarýn ortalamasýný bulan program
+	int t=0,i=1;
+	for(i;i<=100;i++)
+	{
+		t=t+i;
+		
+	}
+	printf("Sayilarin ortalamasi => %d",t/i);
+	getch();
+}
