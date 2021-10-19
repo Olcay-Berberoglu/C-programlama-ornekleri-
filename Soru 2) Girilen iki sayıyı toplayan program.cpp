830b9ea1439995e1fe7497268c2bd577dc86ebfe
Olcay-Berberoglu <84090885+Olcay-Berberoglu@users.noreply.github.com>
2021-10-19 15:25:10 +0300

    Girilen iki sayıyı toplayan program

---
+++ Soru 2) Girilen iki sayıyı toplayan program.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Girilen iki sayýyý toplayan program
+
+ main (){
+
+		int sayi1,sayi2;
+printf("iki sayi gir: ");
+	scanf("%d %d",&sayi1,&sayi2); // & adres operatoru
+	printf("Toplam= %d", sayi1+sayi2 );
+}
