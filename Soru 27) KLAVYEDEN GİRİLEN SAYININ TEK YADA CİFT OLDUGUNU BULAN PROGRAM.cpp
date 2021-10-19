7ab531b730e15b417c291f990e45612b6439cce5
Olcay-Berberoglu <84090885+Olcay-Berberoglu@users.noreply.github.com>
2021-10-19 16:13:50 +0300

    KLAVYEDEN GİRİLEN SAYININ TEK YADA CİFT OLDUGUNU BULAN PROGRAM

---
+++ Soru 27) KLAVYEDEN GİRİLEN SAYININ TEK YADA CİFT OLDUGUNU BULAN PROGRAM.cpp
@@ -0,0 +1,18 @@
+//  KLAVYEDEN GÝRÝLEN SAYININ TEK YADA CÝFT OLDUGUNU BULAN PROGRAM 
+
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+       
+	   int sayi;
+	   printf("SAYÝ GÝR");
+	   scanf("%d",& sayi);
+	   if(sayi%2==0)
+	   printf("cift sayi");
+	   else
+	   printf("tek sayi");
+	   	
+
+
+
+      }
