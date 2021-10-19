5baec8f03ca7d84c781dc91e04a2712c5bbe7aa0
Olcay-Berberoglu <84090885+Olcay-Berberoglu@users.noreply.github.com>
2021-10-19 16:15:34 +0300

    Kullanıcı tarafından girilen yaş değerine bağlı olarak 18 ve üzeri yaşta olanları YETİŞKİN, diğerlerini ÇOCUK olarak hesaplayan programın

---
+++ Soru 29) Kullanıcı tarafından girilen yaş değerine bağlı olarak 18 ve üzeri yaşta olanları YETİŞKİN, diğerlerini ÇOCUK olarak hesaplayan programın.cpp
@@ -0,0 +1,20 @@
+//    Kullanýcý tarafýndan girilen yaþ deðerine baðlý olarak 18 ve üzeri yaþta olanlarý YETÝÞKÝN, diðerlerini ÇOCUK olarak hesaplayan programýn 
+
+
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+	
+	int yas;
+	printf("YASINIZI GIRINIZ= ");
+	scanf("%d",&yas);
+	if(yas>=18)
+	printf("yetiskin");
+	else
+	printf("cocuk");
+	
+
+	
+
+
+      }
